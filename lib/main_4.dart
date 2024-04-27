357a634c410b9bc37ed1c6b6646202c006203cf8
omer <omermenekse@gmail.com>
2024-04-27 23:25:58 +0300

    yeni 27

--- lib/main_4.dart
+++ lib/main_4.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   //bu bir değer döndürmez ve widget bekler
@@ -8,6 +9,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  String _img1 =
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Snow_Scene_at_Shipka_Pass_1.JPG/1280px-Snow_Scene_at_Shipka_Pass_1.JPG';
+
+  String _img2 = "https://www.yurtgazetesi.com.tr/d/news/218952.jpg";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,11 +29,35 @@ class MyApp extends StatelessWidget {
         //bodyde metin yazbilmek için
         body: Center(
           child: Container(
+            padding: EdgeInsets.all(20),
+            //container içerisindekşşekilin rengi
+            // color: Colors.red,
             child: FlutterLogo(
-              style: FlutterLogoStyle.horizontal,
+              //style: FlutterLogoStyle.stacked,
               size: 64,
-              textColor: Colors.red,
+              //textColor: Colors.red,
             ),
+            //gölge vermek için arka plan için
+            //box decorationda verdiğimiz özellikleri containerda da varsa kullanamayız
+            decoration: BoxDecoration(
+                color: Colors.orange,
+                shape: BoxShape.rectangle,
+                border: Border.all(width: 4, color: Colors.purple),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)),
+                image: DecorationImage(
+                    image: NetworkImage(_img1), fit: BoxFit.cover),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.green,
+                      offset: Offset.fromDirection(10, 20),
+                      blurRadius: 10),
+                  BoxShadow(
+                      color: Colors.black,
+                      offset: Offset(0, -20),
+                      blurRadius: 20)
+                ]),
           ),
         ),
         floatingActionButton: FloatingActionButton(
